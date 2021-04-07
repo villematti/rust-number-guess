ec540c9ff5215cdec6f7c59f13e9de7fdcdaa0b0
Ville-Matti Hakanpää <villematti@villematti.info>
2021-04-07 19:11:57 +0300

    Add initial guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
-use rand::Rdg;
+use rand::Rng;
 use std::io::stdin;
 
 fn main() {
+    let number = rand::thread_rng().gen_range(1, 101);
 
+    loop {
+        println!("Enter your guess: ");
+
+        let mut buffer = String::new();
+
+        match stdin().read_line(&mut buffer)
+        {
+            Ok(_) => {
+                let parsed = buffer.trim_end().parse::<i64>();
+                match parsed {
+                    Ok(guess) => {
+                        if guess < 1 || guess > 100 {
+                            println!("Your guess is out of range");
+                        } else if guess < number {
+                            println!("Your guess is too low!");
+                        } else if guess > number {
+                            println!("Your guess is too high!");
+                        } else if guess == number {
+                            println!("Yes! You are correct! Number was {}!", guess);
+                            return;
+                        }
+                        continue;
+                    },
+                    Err(e) => {
+                        println!("Could not read your input. {}. Try again!", e);
+                    }
+                }
+            },
+            Err(_) => continue,
+        }
+    }
 }
